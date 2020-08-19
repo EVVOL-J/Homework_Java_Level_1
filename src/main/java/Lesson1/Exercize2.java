5218f30fe91eb228bb03cde33738b84c7716e7a9
evvol <Keshavoloshin@mail.ru>
2020-08-19 16:06:10 +0300

    Maked ex2

---
+++ src/main/java/Lesson1/Exercize2.java
@@ -0,0 +1,17 @@
+package Lesson1;
+
+public class Exercize2 {
+    public static void main(String[] args) {
+        //2. Создать переменные всех пройденных типов данных и инициализировать их значения.
+        int intNumber=5;
+        float floatNumber=5.14f;
+        double doubleNumber=3.13;
+        long longNumber=123L;
+        char charSymbol='A';
+        short shortNumber=14;
+        boolean bool=true;
+        String ourString="Homework";
+        final int constant=2;
+        var s=12;
+    }
+}
