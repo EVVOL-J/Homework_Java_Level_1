dd6687c5fbc7ea9043343ee91f08e53c831f22ee
evvol <Keshavoloshin@mail.ru>
2020-08-20 21:04:23 +0300

    Maked All_fixed

--- src/main/java/Lesson2/Exercise4.java
+++ src/main/java/Lesson2/Exercise4.java
@@ -6,20 +6,17 @@ public class Exercise4 {
     //Создать квадратный двумерный целочисленный массив (количество строк и столбцов одинаковое),
     // и с помощью цикла(-ов) заполнить его диагональные элементы единицами;
     public static void main(String[] args) {
-        int arr[][]=new int[6][6];
-        for(int i=0;i<arr.length;i++) arr[i][i]=1;
+        int arr[][] = new int[6][6];
+        for (int i = 0; i < arr.length; i++) arr[i][i] = 1;
 
         //check
         for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j <arr.length; j++) {
+            for (int j = 0; j < arr.length; j++) {
                 System.out.print(arr[i][j] + " ");
             }
             System.out.println();
         }
 
 
-
-
-
-        }
+    }
 }
--- src/main/java/Lesson2/Exercise7.java
+++ src/main/java/Lesson2/Exercise7.java
@@ -10,17 +10,19 @@ public class Exercise7 {
     // [ 3, 5, 6, 1] при n = -2 (на два влево) -> [ 6, 1, 3, 5 ].
     // При каком n в какую сторону сдвиг можете выбирать сами.
     public static void main(String[] args) {
-        int arr[]={1,2,3,4,5,6,7,8,9,10};
-        int n=7;
-        System.out.println(Arrays.toString(shift(arr,n)));
+        int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int n = 7;
+        System.out.println(Arrays.toString(shift(arr, n)));
 
     }
-    private static int[] shift(int arr[], int n){
-        n%=arr.length;// для сокращения n
-        if(Math.abs(n)>arr.length/2) n-=arr.length*n/Math.abs(n);//если сдвиг больше чем половина массива, то сдвигаем в другую сторону
+
+    private static int[] shift(int arr[], int n) {
+        n %= arr.length;// для сокращения n
+        if (Math.abs(n) > arr.length / 2)
+            n -= arr.length * n / Math.abs(n);//если сдвиг больше чем половина массива, то сдвигаем в другую сторону
         int safeNumber;
-        System.out.println("Эквивалентное n="+n);
-        if(n!=0) {
+        System.out.println("Эквивалентное n=" + n);
+        if (n != 0) {
             if (n > 0) {
                 for (int i = 0; i < n; i++) {
                     safeNumber = arr[arr.length - 1];
@@ -35,8 +37,8 @@ public class Exercise7 {
                 }
             }
         }
-        return  arr;
-        }
-
+        return arr;
     }
+
+}
 
