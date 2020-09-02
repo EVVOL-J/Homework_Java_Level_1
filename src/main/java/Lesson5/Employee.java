4a47a0ba7e10deb547fbe60d4db900a852d627f5
evvol <Keshavoloshin@mail.ru>
2020-09-02 07:09:37 +0300

    Lesson 5 made

---
+++ src/main/java/Lesson5/Employee.java
@@ -0,0 +1,35 @@
+package Lesson5;
+
+public class Employee {
+    private String fullName;
+    private String position;
+    private String email;
+    private String telefonNumber;
+    private int salary;
+    private int age;
+
+    Employee() {
+        this(30);
+    }
+    Employee(int age){
+        this("Nasarishin Nikita Andreevich", "progremmer", "nasar@mail.ru", "+7(985)-171-30-00", 60000, age);
+
+    }
+
+    Employee(String fullName, String position, String email, String telefonNumber, int salary, int age) {
+        this.fullName = fullName;
+        this.position = position;
+        this.email = email;
+        this.telefonNumber = telefonNumber;
+        this.salary = salary;
+        this.age = age;
+    }
+
+    void info() {
+        System.out.println("ФИО: " + this.fullName + "; Должность: " + this.position + "; Эмэйл: " + this.email + "; Телефонный номер: " + this.telefonNumber + "; Зарплата: " + this.salary + "; Возраст: " + this.age);
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
