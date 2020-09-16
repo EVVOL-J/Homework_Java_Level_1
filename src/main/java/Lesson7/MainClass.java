e4d21ff307ee8c5958b01a838b2c60cb54a3077b
evvol <Keshavoloshin@mail.ru>
2020-09-16 09:46:10 +0300

    Lesson 7 made All

---
+++ src/main/java/Lesson7/MainClass.java
@@ -0,0 +1,27 @@
+package Lesson7;
+
+import java.util.Random;
+
+public class MainClass {
+    public static void main(String[] args) {
+        Cat[] cat = new Cat[5];
+        Plate plate = new Plate(5);
+        Random random=new Random();
+
+        for (int i = 0; i<cat.length; i++){  //не использовал foreach т к нужен был индекс для создания имени
+            cat[i]=new Cat("Барсик"+i, 1+random.nextInt(5));
+        }
+        plate.info();
+        for (Cat caty:cat){
+           caty.eat(plate);
+           plate.info();
+           if(!caty.isHungry()) plate.addFood(random.nextInt(6));
+            System.out.println("----------------------");
+
+        }
+
+
+
+
+    }
+}
---
+++ src/main/java/Lesson7/Plate.java
@@ -0,0 +1,36 @@
+package Lesson7;
+
+public class Plate {
+    private int food;
+
+    public Plate(int food){
+        this.food=food;
+    }
+
+    public void info(){
+        System.out.println("Plate: " + food);
+    }
+
+    public int getFood() {
+        return food;
+    }
+
+    public void setFood(int food) {
+        this.food = food;
+    }
+
+    public boolean decreaseFood(int n){
+        if(food-n>=0) {
+            food -= n;
+            return true;
+        }
+        else {System.out.println("В тарелке недостаточно еды");
+        return false;}
+    }
+
+    public void addFood(int add_Food){
+        food+=add_Food;
+        System.out.println("В тарелку добавлена еда "+ add_Food);
+        info();
+    }
+}
